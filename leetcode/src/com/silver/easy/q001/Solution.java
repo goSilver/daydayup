e29bda63f57c302f3f6989c4e141959f55a39989
chenshaohua <silverchen961@foxmail.com>
2019-09-26 10:13:24 +0800

    添加注释

--- leetcode/src/com/silver/easy/q001/Solution.java
+++ leetcode/src/com/silver/easy/q001/Solution.java
@@ -9,6 +9,13 @@ import java.util.HashMap;
  * @author csh
  */
 public class Solution {
+    /**
+     * 蛮力法：双重循环遍历数组，如果找到相加之和为target，则返回下标数组
+     *
+     * @param nums 数组
+     * @param target 目标之和
+     * @return 下标数组
+     */
 //    public int[] twoSum(int[] nums, int target) {
 //        for (int i = 0; i < nums.length; ++i) {
 //            for (int j = i + 1; j < nums.length; ++j) {
@@ -20,10 +27,16 @@ public class Solution {
 //        return null;
 //    }
 
+    /**
+     *
+     * @param nums 数组
+     * @param target 目标之和
+     * @return 下标数组
+     */
     public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>(8);
-        for (int i = 0; i < len; ++i) {
+        for (int i = 0; i < len; i++) {
             final Integer value = map.get(nums[i]);
             if (value != null) {
                 return new int[] { value, i };
