d6a7abfa654b534b5acb307eabe2bcc54cdaabe6
chenshaohua <silverchen961@foxmail.com>
2019-09-29 14:23:38 +0800

    有效的括号

---
+++ leetcode/src/com/silver/easy/q020/Solution.java
@@ -0,0 +1,16 @@
+package com.silver.easy.q020;
+
+/**
+ * 有效的括号
+ *
+ * @author csh
+ * @date 2019/9/29 14:15
+ */
+public class Solution {
+    public boolean isValid(String str){
+        char[] stack = new char[str.length()+1];
+        int top = 1;
+
+    }
+
+}
