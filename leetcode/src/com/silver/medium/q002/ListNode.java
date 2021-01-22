85139cb6a190b8aa252911b0b5aed2efa3ec8e05
chenshaohua <silverchen961@foxmail.com>
2021-01-22 14:26:58 +0800

    链表操作

---
+++ leetcode/src/com/silver/medium/q002/ListNode.java
@@ -0,0 +1,41 @@
+package com.silver.medium.q002;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * @author Chen ShaoHua
+ * @date 2020/6/3
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
+
+class Solution2 {
+
+    public static void main(String[] args) {
+//        Map<String, Integer> map = new HashMap();
+//        map.put("aaa", 1);
+//        map.put("bbb", 1);
+//        map.put("ccc", 0);
+//
+//        map = map.entrySet().stream()
+//                .filter(o -> o.getValue().equals(1))
+//                .collect(Collectors.toMap((e) -> (String) e.getKey(),
+//                        (e) -> e.getValue()));
+        BigDecimal a = new BigDecimal("-1.00");
+        BigDecimal b = new BigDecimal("-1.00");
+        System.out.println(a.equals(b));
+    }
+
+
+}
---
+++ leetcode/src/com/silver/medium/q002/Solution.java
@@ -0,0 +1,15 @@
+package com.silver.medium.q002;
+
+/**
+ * @author Chen ShaoHua
+ * @date 2020/6/3
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+}
