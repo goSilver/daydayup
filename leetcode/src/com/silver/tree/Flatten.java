fe433aa530a276e824dc89951a6182cdb22fbb37
SONGUKU <silverchen961@foxmail.com>
2021-01-24 12:41:20 +0800

    将以 root 为根的树拉平为链表

--- leetcode/src/com/silver/tree/Flatten.java
+++ leetcode/src/com/silver/tree/Flatten.java
@@ -20,9 +20,11 @@ public class Flatten {
         // 处理已经拉平的左右字树
         TreeNode right = root.getRight();
 
+        // 将左子树作为右子树
         root.setRight(root.getLeft());
         root.setLeft(null);
 
+        // 3、将原先的右子树接到当前右子树的末端
         TreeNode temp = root;
         while (temp.getRight() != null) {
             temp = temp.getRight();
