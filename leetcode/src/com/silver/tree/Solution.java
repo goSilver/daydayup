b427e915972e6852f50a363513b54601c6add491
SONGUKU <silverchen961@foxmail.com>
2021-01-24 11:19:31 +0800

    原地反转二叉树

--- leetcode/src/com/silver/tree/Solution.java
+++
@@ -1,7 +0,0 @@
-package com.silver.tree;
-
-public class Solution {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ leetcode/src/com/silver/tree/TreeNode.java
@@ -0,0 +1,39 @@
+package com.silver.tree;
+
+/**
+ * @author csh
+ * @date 2021/1/24
+ */
+public class TreeNode {
+
+    private int val;
+
+    private TreeNode left;
+
+    private TreeNode right;
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+}
