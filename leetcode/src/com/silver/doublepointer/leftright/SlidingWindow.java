02b1288b6b183e8800dd84a9ec37ebca5ad4bede
SONGUKU <silverchen961@foxmail.com>
2021-01-30 17:49:25 +0800

    找所有字母异位词

--- leetcode/src/com/silver/doublepointer/leftright/SlidingWindow.java
+++ leetcode/src/com/silver/doublepointer/leftright/SlidingWindow.java
@@ -1,6 +1,8 @@
 package com.silver.doublepointer.leftright;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 双指针之滑动窗口技巧
@@ -116,5 +118,58 @@ public class SlidingWindow {
         }
         // 未找到合法字串
         return false;
+    }
+
+    /**
+     * 找所有字母异位词
+     * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串
+     * 返回这些子串的起始索引。
+     * https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/
+     *
+     * @param s 字符串s
+     * @param t 字符串t
+     * @return 符合条件的字串的起始索引
+     */
+    private List<Integer> findAnagrams(String s, String t) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+
+        List<Integer> res = new ArrayList<>();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            // 进行窗口内一系列数据的更新
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 判断左侧窗口是否需要收缩
+            while (right - left >= t.length()) {
+
+                // 当窗口符合条件时，把起始索引加入 res
+                if (valid == need.size()) res.add(left);
+
+                char c1 = s.charAt(left);
+                left++;
+                // 进行窗口内一系列数据更新
+                if (need.containsKey(c1)) {
+                    window.put(c1, window.getOrDefault(c1, 0)-1);
+                    if (window.get(c1).equals(need.get(c1))) {
+                        valid--;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
