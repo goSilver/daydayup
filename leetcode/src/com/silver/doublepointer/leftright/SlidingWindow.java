577dfe4bf4a7f53d83c4e45a2d7a90807ba79a33
SONGUKU <silverchen961@foxmail.com>
2021-01-30 17:32:50 +0800

    字符串排列

--- leetcode/src/com/silver/doublepointer/leftright/SlidingWindow.java
+++ leetcode/src/com/silver/doublepointer/leftright/SlidingWindow.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 
 /**
  * 双指针之滑动窗口技巧
- * https://leetcode-cn.com/problems/minimum-window-substring/
  *
  * @author csh
  * @date 2021/01/30
@@ -19,6 +18,7 @@ public class SlidingWindow {
 
     /**
      * 最小覆盖字串
+     * https://leetcode-cn.com/problems/minimum-window-substring/
      *
      * @param s 字符串s
      * @param t 字符串t
@@ -67,4 +67,54 @@ public class SlidingWindow {
         return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
     }
 
+    /**
+     * 字符串排列
+     * https://leetcode-cn.com/problems/permutation-in-string/
+     *
+     * @param s 字符串s
+     * @param t 字符串t
+     * @return s中是否存在t的排列
+     */
+    private boolean checkInclusion(String s, String t) {
+        HashMap<Character, Integer> need = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int valid = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            // 进行窗口内一系列数据的更新
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+
+                if (window.get(c).equals(need.get(c))) {
+                    valid++;
+                }
+            }
+
+            // 判断左侧窗口是否需要收缩
+            while (right - left >= t.length()) {
+
+                // 判断是否找到合法字串
+                if (valid == need.size()) return true;
+
+                char c1 = s.charAt(left);
+                left++;
+                // 进行窗口内一系列数据更新
+                if (need.containsKey(c1)) {
+                    window.put(c1, window.getOrDefault(c1, 0)-1);
+                    if (window.get(c1).equals(need.get(c1))) {
+                        valid--;
+                    }
+                }
+            }
+        }
+        // 未找到合法字串
+        return false;
+    }
 }
