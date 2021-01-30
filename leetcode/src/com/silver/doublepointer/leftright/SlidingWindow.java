760de7552a47327ccc8e8323905f667575ed1f50
SONGUKU <silverchen961@foxmail.com>
2021-01-30 19:02:56 +0800

    最长无重复子串

--- leetcode/src/com/silver/doublepointer/leftright/SlidingWindow.java
+++ leetcode/src/com/silver/doublepointer/leftright/SlidingWindow.java
@@ -172,4 +172,30 @@ public class SlidingWindow {
         }
         return res;
     }
+
+    /**
+     * 最长无重复子串
+     * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+     *
+     * @param s
+     * @return
+     */
+    private int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            right++;
+            // 判断是否应该收缩左边窗口
+            while (window.get(c) > 1) {
+                char c1 = s.charAt(left);
+                left++;
+                window.put(c1, window.getOrDefault(c1, 0) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
 }
