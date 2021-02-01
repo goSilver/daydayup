1036882e0a76395d4f02083151dfc3f33d91cfc4
chenshaohua <silverchen961@foxmail.com>
2021-02-01 17:10:50 +0800

    二维数组中的查找

--- leetcode/src/com/silver/sword4offer/q3_q10/q04.java
+++ leetcode/src/com/silver/sword4offer/q3_q10/q04.java
@@ -9,7 +9,12 @@ package com.silver.sword4offer.q3_q10;
 public class q04 {
 
     public static void main(String[] args) {
-        int[][] arr = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        int[][] arr = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11,12,13,14,15},
+                {16,17,18,19,20},
+                {21,22,23,24,25}};
         q04 q04 = new q04();
         boolean res = q04.findNumberIn2DArray(arr, 19);
         System.out.println(res);
