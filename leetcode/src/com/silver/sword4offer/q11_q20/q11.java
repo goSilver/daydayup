e27e3764c48ebfda8b04820f768afb08926f2974
SONGUKU <silverchen961@foxmail.com>
2021-02-02 20:21:50 +0800

    旋转数组的最小数字

---
+++ leetcode/src/com/silver/sword4offer/q11_q20/q11.java
@@ -0,0 +1,12 @@
+package com.silver.sword4offer.q11_q20;
+
+/**
+ * @author csh
+ * @date 2021/2/1
+ **/
+public class q11 {
+    public int minArray(int[] numbers) {
+        int mid = numbers.length / 2;
+        return numbers[mid];
+    }
+}
