2086f7792d9cac59878ab9c2d6970cbf1ee436ab
SONGUKU <silverchen961@foxmail.com>
2021-02-03 23:03:40 +0800

    子集

---
+++ leetcode/src/com/silver/labuladong/backtrack/dfs/SubSets.java
@@ -0,0 +1,30 @@
+package com.silver.labuladong.backtrack.dfs;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 输入一个不包含重复数字的数组，要求算法输出这些数字的所有子集。
+ *
+ * @author csh
+ * @date 2021/2/3
+ **/
+public class SubSets {
+
+    private List<List<Integer>> res = new LinkedList<>();
+
+    private List<List<Integer>> subSets(int[] nums) {
+        LinkedList<Integer> track = new LinkedList<>();
+        backtrack(nums, 0, track);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, LinkedList<Integer> track) {
+        res.add(track);
+        for (int i = start; i < nums.length; i++) {
+            track.add(nums[i]);
+            backtrack(nums, i + 1, track);
+            track.removeLast();
+        }
+    }
+}
