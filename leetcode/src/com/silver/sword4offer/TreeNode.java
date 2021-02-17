78d079c8a40c962a2b617419b6faf9fbcfd846d3
SONGUKU <silverchen961@foxmail.com>
2021-02-17 22:35:54 +0800

    二叉树节点

---
+++ leetcode/src/com/silver/sword4offer/TreeNode.java
@@ -0,0 +1,15 @@
+package com.silver.sword4offer;
+
+/**
+ * @author csh
+ * @date 2021/2/17
+ **/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
