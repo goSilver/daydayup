8ce79f13ff30e0e63d4f18177e1bbabf5c9fc74e
SONGUKU <silverchen961@foxmail.com>
2021-02-21 17:19:06 +0800

    双向链表节点定义

---
+++ leetcode/src/com/silver/leetcode/datastructure/Node.java
@@ -0,0 +1,14 @@
+package com.silver.leetcode.datastructure;
+
+/**
+ * 双向链表节点
+ *
+ * @author csh
+ * @date 2021/2/21
+ **/
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+}
