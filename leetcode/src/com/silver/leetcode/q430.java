a1188561415b17e15a9ce90926476ab378eefa22
SONGUKU <silverchen961@foxmail.com>
2021-02-21 23:14:32 +0800

    扁平化多级双向链表

---
+++ leetcode/src/com/silver/leetcode/q430.java
@@ -0,0 +1,21 @@
+package com.silver.leetcode;
+
+import com.silver.leetcode.datastructure.Node;
+
+/**
+ * 扁平化多级双向链表
+ *
+ * @author csh
+ * @date 2021/2/21
+ **/
+public class q430 {
+    public Node flatten(Node head) {
+        if (head.child != null) {
+            head = head.next;
+        } else {
+            flatten(head.child);
+
+        }
+        return null;
+    }
+}
