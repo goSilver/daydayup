34dfa85e5a637c4f5375304e73b3702b1ea6a9a5
SONGUKU <silverchen961@foxmail.com>
2021-02-22 22:06:21 +0800

    三数之和

---
+++ leetcode/src/com/silver/leetcode/q15.java
@@ -0,0 +1,37 @@
+package com.silver.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 三数之和
+ *
+ * @author csh
+ * @date 2021/2/22
+ **/
+public class q15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int k = 0; k < nums.length - 2; k++) {
+            if (nums[k] > 0 ) break;
+            if (k>0 && nums[k]==nums[k-1]) continue;
+            int i = k + 1, j = nums.length - 1;
+            while(i < j){
+                int sum = nums[k] + nums[i] + nums[j];
+                if(sum < 0){
+                    while(i < j && nums[i] == nums[++i]);
+                } else if (sum > 0) {
+                    while(i < j && nums[j] == nums[--j]);
+                } else {
+                    res.add(new ArrayList<>(Arrays.asList(nums[k], nums[i], nums[j])));
+                    while(i < j && nums[i] == nums[++i]);
+                    while(i < j && nums[j] == nums[--j]);
+                }
+            }
+        }
+        return res;
+    }
+}
