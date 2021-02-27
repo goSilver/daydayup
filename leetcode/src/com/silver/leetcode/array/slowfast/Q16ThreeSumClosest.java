3b2177c55c44efbe47e23d6656136e9274648465
SONGUKU <silverchen961@foxmail.com>
2021-02-27 23:27:54 +0800

    最接近的三数之和

---
+++ leetcode/src/com/silver/leetcode/array/slowfast/Q16ThreeSumClosest.java
@@ -0,0 +1,43 @@
+package com.silver.leetcode.array.slowfast;
+
+import java.util.Arrays;
+
+/**
+ * 最接近的三数之和
+ *
+ * @author csh
+ * @date 2021/2/22
+ **/
+public class Q16ThreeSumClosest {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,2,1,4};
+        int target = 1;
+        Q16ThreeSumClosest main = new Q16ThreeSumClosest();
+        int res = main.threeSumClosest(nums, target);
+        System.out.println(res);
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        int best = 10000000;
+        for (int i = 0; i < nums.length; i++) {
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                best = Math.abs(sum - target) > Math.abs(best - target) ? best : sum;
+
+                if (sum > target) {
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    return target;
+                }
+            }
+        }
+
+        return best;
+    }
+}
--- leetcode/src/com/silver/leetcode/q16.java
+++
@@ -1,9 +0,0 @@
-package com.silver.leetcode;
-
-/**
- * @author csh
- * @date 2021/2/22
- **/
-public class q16 {
-
-}
