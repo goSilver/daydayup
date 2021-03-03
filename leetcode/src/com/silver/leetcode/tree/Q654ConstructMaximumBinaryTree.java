59fa0feb9905b4bda53f8aa201649f3a7aa035e8
SONGUKU <silverchen961@foxmail.com>
2021-03-03 23:10:40 +0800

    最大二叉树

---
+++ leetcode/src/com/silver/leetcode/tree/Q654ConstructMaximumBinaryTree.java
@@ -0,0 +1,39 @@
+package com.silver.leetcode.tree;
+
+import com.silver.sword4offer.TreeNode;
+
+/**
+ * 最大二叉树
+ *
+ * @author csh
+ * @date 2021/3/3
+ **/
+public class Q654ConstructMaximumBinaryTree {
+    public static void main(String[] args) {
+        Q654ConstructMaximumBinaryTree main = new Q654ConstructMaximumBinaryTree();
+        int[] nums = new int[]{3,2,1,6,0,5};
+        TreeNode treeNode = main.constructMaximumBinaryTree(nums);
+
+    }
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        return constructMaximumBinaryTree(nums, 0, nums.length);
+    }
+
+    private TreeNode constructMaximumBinaryTree(int[] nums, int begin, int end) {
+        if (begin >= end) return null;
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = begin; i < end; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+        TreeNode root = new TreeNode(max);
+
+        root.left = constructMaximumBinaryTree(nums, begin, index);
+        root.right = constructMaximumBinaryTree(nums, index+1, end);
+
+        return root;
+    }
+}
