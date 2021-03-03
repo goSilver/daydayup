94306272bd2139199c881e28dd0ba725cd7495cf
SONGUKU <silverchen961@foxmail.com>
2021-03-03 22:19:23 +0800

    二叉树展开为链表

---
+++ leetcode/src/com/silver/leetcode/tree/Q114Flatten.java
@@ -0,0 +1,35 @@
+package com.silver.leetcode.tree;
+
+import com.silver.sword4offer.TreeNode;
+
+/**
+ * 二叉树展开为链表
+ *
+ * @author csh
+ * @date 2021/3/3
+ **/
+public class Q114Flatten {
+    public void flatten(TreeNode root) {
+        // base case
+        if (root == null) return;
+
+        flatten(root.left);
+        flatten(root.right);
+
+        // 后序遍历位置
+        // 1、左右子树已经被拉平成一条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        // 2、将左子树作为右子树
+        root.left = null;
+        root.right = left;
+
+        // 3、将原先的右子树接到当前右子树的末端
+        TreeNode p = root;
+        while (p.right != null) {
+            p = p.right;
+        }
+        p.right = right;
+    }
+}
