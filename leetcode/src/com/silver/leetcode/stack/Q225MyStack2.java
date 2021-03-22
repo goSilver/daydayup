c562db5e1707b31fd73ea456a89112175169e539
SONGUKU <silverchen961@foxmail.com>
2021-03-22 21:44:23 +0800

    用队列实现栈

---
+++ leetcode/src/com/silver/leetcode/stack/Q225MyStack2.java
@@ -0,0 +1,43 @@
+package com.silver.leetcode.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 用队列实现栈（一个队列实现版）
+ *
+ * @author csh
+ * @date 2021/3/22
+ */
+public class Q225MyStack2 {
+    Queue<Integer> queue;
+
+    /** Initialize your data structure here. */
+    public Q225MyStack2() {
+        queue = new LinkedList<>();
+    }
+
+    /** Push element x onto stack. */
+    public void push(int x) {
+        int size = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < size; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue.poll();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        return queue.peek();
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
