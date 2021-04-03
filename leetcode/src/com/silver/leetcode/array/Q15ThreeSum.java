96fcbdb022d030404a92c263c269ce65e9327734
SONGUKU <silverchen961@foxmail.com>
2021-04-03 19:32:00 +0800

    三数之和

--- leetcode/src/com/silver/leetcode/array/Q15ThreeSum.java
+++ leetcode/src/com/silver/leetcode/array/Q15ThreeSum.java
@@ -11,23 +11,34 @@ import java.util.List;
  * @date 2021/2/22
  **/
 public class Q15ThreeSum {
+
+    /**
+     * 思路：
+     * 排序 + 双指针
+     *
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         for (int k = 0; k < nums.length - 2; k++) {
-            if (nums[k] > 0 ) break;
-            if (k>0 && nums[k]==nums[k-1]) continue;
+            // 排好序的数组，如果第一个加数就大于零，则后面的必大于零，故三数之和必大于零
+            if (nums[k] > 0) break;
+            // 跳过相同的数字（下同）
+            if (k > 0 && nums[k] == nums[k - 1]) continue;
+            // 前后双指针
             int i = k + 1, j = nums.length - 1;
-            while(i < j){
+            while (i < j) {
                 int sum = nums[k] + nums[i] + nums[j];
-                if(sum < 0){
-                    while(i < j && nums[i] == nums[++i]);
+                if (sum < 0) {
+                    while (i < j && nums[i] == nums[++i]) ;
                 } else if (sum > 0) {
-                    while(i < j && nums[j] == nums[--j]);
+                    while (i < j && nums[j] == nums[--j]) ;
                 } else {
                     res.add(new ArrayList<>(Arrays.asList(nums[k], nums[i], nums[j])));
-                    while(i < j && nums[i] == nums[++i]);
-                    while(i < j && nums[j] == nums[--j]);
+                    while (i < j && nums[i] == nums[++i]) ;
+                    while (i < j && nums[j] == nums[--j]) ;
                 }
             }
         }
