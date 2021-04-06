b53072201acba6f1270e121ad190824e78a6e3e2
SONGUKU <silverchen961@foxmail.com>
2021-04-06 21:29:15 +0800

    股票买卖的最佳时机

---
+++ leetcode/src/com/silver/leetcode/array/Q121MaxProfit.java
@@ -0,0 +1,31 @@
+package com.silver.leetcode.array;
+
+/**
+ * 股票买卖的最佳时机
+ *
+ * @author csh
+ * @date 2021/4/6
+ */
+public class Q121MaxProfit {
+
+    /**
+     * 只有两次交易时机
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice) {
+                minprice = prices[i];
+            } else if (prices[i] - minprice > maxprofit) {
+                maxprofit = prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+
+
+}
