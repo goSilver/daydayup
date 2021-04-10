adf72678e3949049db7449ef9310ea86caac37f1
SONGUKU <silverchen961@foxmail.com>
2021-04-10 19:44:36 +0800

    删除排序数组中的重复项

--- leetcode/src/com/silver/review/array/RemoveDuplicates.java
+++ leetcode/src/com/silver/review/array/RemoveDuplicates.java
@@ -20,4 +20,22 @@ public class RemoveDuplicates {
         }
         return slow+1;
     }
+
+    /**
+     * 注意这里和有序数组去重的解法有一个重要不同，
+     * 我们这里是先给nums[slow]赋值然后再给slow++，
+     * 这样可以保证nums[0..slow-1]是不包含值为val的元素的，
+     * 最后的结果数组长度就是slow。
+     */
+    public int removeElement(int[] nums, int val) {
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
 }
