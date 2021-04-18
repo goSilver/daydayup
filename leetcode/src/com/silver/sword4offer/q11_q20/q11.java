7fa2076d10300944e7faa6376c98eac7cb114236
SONGUKU <silverchen961@foxmail.com>
2021-04-18 13:54:15 +0800

    旋转数组的最小数字

--- leetcode/src/com/silver/sword4offer/q11_q20/q11.java
+++ leetcode/src/com/silver/sword4offer/q11_q20/q11.java
@@ -10,13 +10,20 @@ package com.silver.sword4offer.q11_q20;
 public class q11 {
     public int minArray(int[] numbers) {
         int i = 0, j = numbers.length - 1;
+
+        // 当i=j时，跳出循环
         while (i < j) {
             int mid = (i + j) / 2;
+            // 当mid>j，说明结果在mid右边
             if (numbers[mid] > numbers[j]) {
                 i = mid + 1;
+
+            // 当mid<j，说明结果在mid左边
             } else if (numbers[mid] < numbers[j]) {
                 j = mid;
-            } else {
+
+            // 如果mid=j，无法判断在左还是在右，缩小右侧边界继续
+            } else if (numbers[mid] == numbers[j]) {
                 j--;
             }
         }
