e50f5e0d3e0849c3139729b6feac3f7b9733dfb5
SONGUKU <silverchen961@foxmail.com>
2021-05-01 15:10:49 +0800

    左右指针

--- leetcode/src/com/silver/labuladong/catalog/chapter1/doublepointer/LeftRight.java
+++ leetcode/src/com/silver/labuladong/catalog/chapter1/doublepointer/LeftRight.java
@@ -5,7 +5,41 @@ package com.silver.labuladong.catalog.chapter1.doublepointer;
  * @date 2021/5/1
  */
 public class LeftRight {
-    // 二分查找
+    // 寻找左侧边界的二分查找
+    private int leftBound(int[] arr, int target) {
+        if (arr.length < 1) return -1;
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = (left + (right - left)) / 2;
+            if (arr[mid] == target) {
+                right = mid;// 注意
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else if (arr[mid] > target) {
+                right = mid;// 注意
+            }
+        }
+        return left;
+    }
+
+    // 寻找右侧边界的二分查找
+    private int rightBound(int[] arr, int target) {
+        if (arr.length < 1) return -1;
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = (left + (right - left)) / 2;
+            if (arr[mid] == target) {
+                left = mid + 1;// 注意
+            } else if (arr[mid] < target) {
+                left = mid + 1;// 注意
+            } else if (arr[mid] > target) {
+                right = mid;
+            }
+        }
+        return left;
+    }
 
     // 反转数组
     private void reverse(int[] arr) {
@@ -15,7 +49,8 @@ public class LeftRight {
             int temp = arr[left];
             arr[left] = arr[right];
             arr[right] = temp;
-            left++;right--;
+            left++;
+            right--;
         }
     }
 
