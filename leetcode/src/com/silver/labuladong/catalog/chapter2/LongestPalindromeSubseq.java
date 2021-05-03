732fb8b108decd2c57fed6f5f896d0c6d8626e1f
SONGUKU <silverchen961@foxmail.com>
2021-05-03 13:01:46 +0800

    状态压缩解法

--- leetcode/src/com/silver/labuladong/catalog/chapter2/LongestPalindromeSubseq.java
+++ leetcode/src/com/silver/labuladong/catalog/chapter2/LongestPalindromeSubseq.java
@@ -1,5 +1,7 @@
 package com.silver.labuladong.catalog.chapter2;
 
+import java.util.Arrays;
+
 /**
  * 最长回文子序列
  *
@@ -27,5 +29,28 @@ public class LongestPalindromeSubseq {
         }
         // 整个s的最长回文子序列长度
         return dp[0][n - 1];
+    }
+
+    // 对二维dp数组进行状态压缩的解法
+    private int longestPalindromeSubseqZip(String s) {
+        int n = s.length();
+        int[] dp = new int[n];
+        // base case;一维数组全部初始化为1
+        Arrays.fill(dp, 1);
+        for (int i = n - 2; i >= 0; i--) {
+            // 存储dp[i+1][j-1]的变量
+            int pre = 0;
+            for (int j = i + 1; j < n; j++) {
+                int temp = dp[j];
+                if (s.charAt(i) == s.charAt(j))
+                    // dp[i][j] = dp[i+1][j-1]+2
+                    dp[j] = pre + 2;
+                else
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                // 到下一轮循环，pre就是dp[i+1][j-1]了
+                pre = temp;
+            }
+        }
+        return dp[n - 1];
     }
 }
