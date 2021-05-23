3f00e687b4cdcb9e6d28cc754d7ab2600f5cde9b
SONGUKU <silverchen961@foxmail.com>
2021-05-23 20:48:53 +0800

    二叉树的深度

--- leetcode/src/com/silver/review/tree/MinDepth.java
+++ leetcode/src/com/silver/review/tree/MinDepth.java
@@ -3,10 +3,12 @@ package com.silver.review.tree;
 import com.silver.sword4offer.TreeNode;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
- * 二叉树的最小深度
+ * 二叉树的最大、最小深度
+ * 重点：求最大时，初始depth为0；求最小时，初始depth为1
  *
  * @author csh
  * @date 2021/4/11
@@ -21,20 +23,39 @@ public class MinDepth {
         queue.offer(root);
 
         while (!queue.isEmpty()) {
+            // 重点
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
-                if (node.left == null && node.right == null)
-                    return depth;
+//                if (node.left == null && node.right == null)
+//                    return depth;
 
                 if (node.left != null)
                     queue.offer(node.left);
                 if (node.right != null)
                     queue.offer(node.right);
             }
+            // 重点
             depth++;
         }
          return depth;
     }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        List<TreeNode> queue = new LinkedList<TreeNode>() {{ add(root); }}, tmp;
+        int res = 0;
+        while(!queue.isEmpty()) {
+            tmp = new LinkedList<>();
+            for(TreeNode node : queue) {
+                if(node.left != null) tmp.add(node.left);
+                if(node.right != null) tmp.add(node.right);
+            }
+            queue = tmp;
+            res++;
+        }
+        return res;
+    }
+
 
 }
