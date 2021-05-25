da9c8f9eaf0ab36b33612ed02a99af1be6a43a51
SONGUKU <silverchen961@foxmail.com>
2021-05-25 22:16:21 +0800

    最长公共子序列

--- leetcode/src/com/silver/labuladong/catalog/chapter2/LengthOfLCS.java
+++ leetcode/src/com/silver/labuladong/catalog/chapter2/LengthOfLCS.java
@@ -11,7 +11,7 @@ public class LengthOfLCS {
     private int longestCommonSubsequence(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
-        // 定义：dp[i][j]表示s1[0..i]和s[0..j]中最长公共子序列的长度
+        // 定义：dp[i][j]表示s1[0..i-1]和s2[0..j-1]中最长公共子序列的长度
         int[][] dp = new int[m + 1][n + 1];
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
