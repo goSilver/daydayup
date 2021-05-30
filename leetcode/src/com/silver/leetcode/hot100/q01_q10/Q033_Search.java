21aced685c9d7b8806711adc70fc8b4153ffe0ac
SONGUKU <silverchen961@foxmail.com>
2021-05-30 22:29:28 +0800

    旋转数组中寻找一个数

---
+++ leetcode/src/com/silver/leetcode/hot100/q01_q10/Q033_Search.java
@@ -0,0 +1,48 @@
+package com.silver.leetcode.hot100.q01_q10;
+
+/**
+ * @author csh
+ * @date 2021/5/30
+ **/
+public class Q033_Search {
+
+    /**
+     * 旋转数组中寻找一个数
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // 重点。判断搜索区间
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
