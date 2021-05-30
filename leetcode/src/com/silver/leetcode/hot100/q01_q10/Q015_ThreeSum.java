1fdce93952ded0b09768168753d2a1493671c9ca
SONGUKU <silverchen961@foxmail.com>
2021-05-30 20:03:13 +0800

    三数之和

---
+++ leetcode/src/com/silver/leetcode/hot100/q01_q10/Q015_ThreeSum.java
@@ -0,0 +1,48 @@
+package com.silver.leetcode.hot100.q01_q10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author csh
+ * @date 2021/5/30
+ **/
+public class Q015_ThreeSum {
+
+    /**
+     * 是否存在三个数和为0
+     * 思路：for循环加双指针，难点是去重
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            // 注意
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+                    // 注意
+                    while(left < right && nums[left] == nums[++left]);
+                    while(left < right && nums[right] == nums[--right]);
+
+                } else if (sum > 0) {
+                    // 注意
+                    while(left < right && nums[right] == nums[--right]);
+                } else if (sum < 0) {
+                    // 注意
+                    while(left < right && nums[left] == nums[++left]);
+                }
+            }
+        }
+        return res;
+    }
+}
