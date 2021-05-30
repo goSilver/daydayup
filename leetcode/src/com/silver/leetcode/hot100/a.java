a8c57588684ee61e44db3ca99a238659ab06cab5
SONGUKU <silverchen961@foxmail.com>
2021-05-30 14:38:29 +0800

    两数之和

---
+++ leetcode/src/com/silver/leetcode/hot100/a.java
@@ -0,0 +1,8 @@
+package com.silver.leetcode.hot100;
+
+/**
+ * @author csh
+ * @date 2021/5/30
+ **/
+public class a {
+}
--- leetcode/src/com/silver/leetcode/hot100/q01_q10/Q001_TwoSum.java
+++ leetcode/src/com/silver/leetcode/hot100/q01_q10/Q001_TwoSum.java
@@ -1,4 +1,4 @@
-package com.silver.leetcode.hot100;
+package com.silver.leetcode.hot100.q01_q10;
 
 import java.util.Arrays;
 import java.util.HashMap;
