4f0eca0287d8bb7ede8e7089e0ae91f3fdabaaea
SONGUKU <silverchen961@foxmail.com>
2021-05-30 17:16:06 +0800

    最长回文字串

---
+++ leetcode/src/com/silver/leetcode/hot100/q01_q10/Q005_LongestPalindrome.java
@@ -0,0 +1,36 @@
+package com.silver.leetcode.hot100.q01_q10;
+
+/**
+ * @author csh
+ * @date 2021/5/30
+ **/
+public class Q005_LongestPalindrome {
+
+    /**
+     * 最长回文字串
+     * 思路：中心扩展算法
+     * 时间复杂度：O(n^2)
+     *
+     * @param s 字符串
+     * @return 最长回文串
+     */
+    public String longestPalindrome(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+
+            res = s1.length() > res.length() ? s1 : res;
+            res = s2.length() > res.length() ? s2 : res;
+        }
+        return res;
+    }
+
+    private String palindrome(String str, int l, int r) {
+        while (l >= 0 && r < str.length() && str.charAt(l) == str.charAt(r)) {
+            l--;
+            r++;
+        }
+        return str.substring(l + 1, r);
+    }
+}
