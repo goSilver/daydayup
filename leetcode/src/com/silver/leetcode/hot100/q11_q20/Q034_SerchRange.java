4e337acc4c3e3a762effba5a9f96e6e1f7a492da
SONGUKU <silverchen961@foxmail.com>
2021-05-30 22:44:14 +0800

    在排序数组中查找元素的第一个和最后一个位置

---
+++ leetcode/src/com/silver/leetcode/hot100/q11_q20/Q034_SerchRange.java
@@ -0,0 +1,37 @@
+package com.silver.leetcode.hot100.q11_q20;
+
+/**
+ * @author csh
+ * @date 2021/5/30
+ **/
+public class Q034_SerchRange {
+    /**
+     *  在排序数组中查找元素的第一个和最后一个位置
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
