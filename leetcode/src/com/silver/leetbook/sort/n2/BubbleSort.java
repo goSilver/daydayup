fdead9efec9aadd70073c95a1eda9a58520780e9
SONGUKU <silverchen961@foxmail.com>
2021-06-18 00:17:33 +0800

    冒泡排序

--- leetcode/src/com/silver/leetbook/sort/n2/BubbleSort.java
+++ leetcode/src/com/silver/leetbook/sort/n2/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort {
     public static void main(String[] args) {
         BubbleSort main = new BubbleSort();
         int[] arr = new int[]{1,4,3,5,2};
-        main.bubbleSort(arr);
+        main.bubbleSort3(arr);
         for (int i : arr) {
             System.out.print(i);
         }
@@ -24,6 +24,48 @@ public class BubbleSort {
                 if (arr[j] > arr[j+1])
                     swap(arr, j, j+1);
             }
+        }
+    }
+
+    private void bubbleSort2(int[] arr) {
+        // 初始时 swapped 为 true，否则排序过程无法启动
+        boolean swapped = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            // 如果没有发生过交换，说明剩余部分已经有序，排序完成
+            if (!swapped) break;
+            // 设置 swapped 为 false，如果发生交换，则将其置为 true
+            swapped = false;
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    // 如果左边的数大于右边的数，则交换，保证右边的数字最大
+                    swap(arr, j, j+1);
+                    // 表示发生了交换
+                    swapped = true;
+                }
+            }
+        }
+    }
+
+    private void bubbleSort3(int[] arr) {
+        boolean swapped = true;
+        // 最后一个没有经过排序的元素的下标
+        int lastUnsortedElementIndex = arr.length - 1;
+        // 上次发生交换的位置
+        int swappedIndex = -1;
+        while (swapped) {
+            swapped = false;
+            for (int i = 0; i < lastUnsortedElementIndex; i++) {
+                if (arr[i] > arr[i+1]) {
+                    // 如果左边的数大于右边的数，则交换，保证右边的数字最大
+                    swap(arr, i, i+1);
+                    // 表示发生了交换
+                    swapped = true;
+                    // 更新交换位置
+                    swappedIndex = i;
+                }
+            }
+            // 最后一个没有发生交换的元素的下标就是下一次发生交换的位置
+            lastUnsortedElementIndex = swappedIndex;
         }
     }
 
