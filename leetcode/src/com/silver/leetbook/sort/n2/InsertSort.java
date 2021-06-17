52f1203671fcff73e082f927e50bef53ae6d49c5
SONGUKU <silverchen961@foxmail.com>
2021-06-18 01:05:46 +0800

    插入排序

---
+++ leetcode/src/com/silver/leetbook/sort/n2/InsertSort.java
@@ -0,0 +1,36 @@
+package com.silver.leetbook.sort.n2;
+
+/**
+ * 插入排序
+ * @author csh
+ * @date 2021/6/18
+ **/
+public class InsertSort {
+    public static void main(String[] args) {
+        InsertSort main = new InsertSort();
+        int[] arr = new int[]{1,4,3,5,2};
+        main.insertSort(arr);
+        for (int i : arr) {
+            System.out.print(i);
+        }
+    }
+    private void insertSort(int[] arr) {
+        // 从第二个数字开始，插入
+        for (int i = 1; i < arr.length; i++) {
+            // j 记录当前数字下标
+            int j = i;
+            // 当前数字比前一个数字要小，进行交换
+            while (j >= 1 && arr[j] < arr[j-1]) {
+                swap(arr, j, j-1);
+                // 更新当前数字下标，继续往前比较
+                j--;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
