2d3457c4c51b5147147fa5e8075804dd87b60e2a
SONGUKU <silverchen961@foxmail.com>
2021-06-19 16:02:55 +0800

    移动零

---
+++ leetcode/src/com/silver/leetbook/sort/n2/BubbleSort/MoveZeros.java
@@ -0,0 +1,26 @@
+package com.silver.leetbook.sort.n2.BubbleSort;
+
+/**
+ * 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * @author csh
+ * @date 2021/6/19
+ **/
+public class MoveZeros {
+
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] == 0) {
+                    swap(nums, j, nums.length-1-i);
+                }
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ leetcode/src/com/silver/leetbook/sort/nlogn/MergeSort.java
@@ -0,0 +1,9 @@
+package com.silver.leetbook.sort.nlogn;
+
+/**
+ * @author csh
+ * @date 2021/6/17
+ **/
+public class MergeSort {
+
+}
