e4056f4cc5e90b5740356636d61bda0099225777
SONGUKU <silverchen961@foxmail.com>
2021-06-20 22:16:30 +0800

    判断是否平衡二叉树

--- leetcode/src/com/silver/sword4offer/q51_q60/q55_MaxDepth.java
+++ leetcode/src/com/silver/sword4offer/q51_q60/q55_MaxDepth.java
@@ -7,28 +7,51 @@ import java.util.List;
 
 /**
  * 二叉树的最大深度
+ *
  * @author csh
  * @date 2021/6/20
  **/
 public class q55_MaxDepth {
     public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if (root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
     public int maxDepth2(TreeNode root) {
-        if(root == null) return 0;
-        List<TreeNode> queue = new LinkedList() {{ add(root); }}, tmp;
+        if (root == null) return 0;
+        List<TreeNode> queue = new LinkedList() {{
+            add(root);
+        }}, tmp;
         int res = 0;
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             tmp = new LinkedList<>();
-            for(TreeNode node : queue) {
-                if(node.left != null) tmp.add(node.left);
-                if(node.right != null) tmp.add(node.right);
+            for (TreeNode node : queue) {
+                if (node.left != null) tmp.add(node.left);
+                if (node.right != null) tmp.add(node.right);
             }
             queue = tmp;
             res++;
         }
         return res;
+    }
+
+    /**
+     * 判断是否平衡二叉树
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    private int recur(TreeNode root) {
+        if (root == null) return 0;
+        int left = recur(root.left);
+        if (left == -1) return -1;
+        int right = recur(root.right);
+        if (right == -1) return -1;
+
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
     }
 }
