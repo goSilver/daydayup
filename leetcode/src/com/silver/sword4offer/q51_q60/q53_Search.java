d9b078d0014fbf681e79cc3e09376acf17b7e6fe
SONGUKU <silverchen961@foxmail.com>
2021-06-20 21:20:47 +0800

    在排序数组中查找数字

---
+++ leetcode/src/com/silver/sword4offer/q51_q60/q53_Search.java
@@ -0,0 +1,34 @@
+package com.silver.sword4offer.q51_q60;
+
+/**
+ * @author csh
+ * @date 2021/6/20
+ **/
+public class q53_Search {
+
+    /**
+     * 在排序数组中查找数字 I
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        return helper(nums, target) - helper(nums, target - 1);
+    }
+
+    private int helper(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        // <=，小于等于
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] <= target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return i;
+    }
+}
