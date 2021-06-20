8cfa3267bfd137666c4fba659504aa306d612df8
SONGUKU <silverchen961@foxmail.com>
2021-06-20 23:21:17 +0800

    翻转单词顺序

---
+++ leetcode/src/com/silver/sword4offer/q51_q60/q58_ReverseWords.java
@@ -0,0 +1,23 @@
+package com.silver.sword4offer.q51_q60;
+
+/**
+ * 翻转单词顺序
+ * @author csh
+ * @date 2021/6/20
+ **/
+public class q58_ReverseWords {
+
+    public String reverseWords(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while(i >= 0) {
+            while(i >= 0 && s.charAt(i) != ' ') i--; // 搜索首个空格
+            res.append(s, i + 1, j + 1).append(" "); // 添加单词
+            while(i >= 0 && s.charAt(i) == ' ') i--; // 跳过单词间空格
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
+    }
+
+}
