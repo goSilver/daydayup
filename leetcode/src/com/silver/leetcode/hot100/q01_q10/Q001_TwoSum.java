03fcb154648ac5b7acb58f0fb6540fb4fb8ee154
SONGUKU <silverchen961@foxmail.com>
2021-06-21 22:15:58 +0800

    两数之和

--- leetcode/src/com/silver/leetcode/hot100/q01_q10/Q001_TwoSum.java
+++ leetcode/src/com/silver/leetcode/hot100/q01_q10/Q001_TwoSum.java
@@ -63,12 +63,12 @@ public class Q001_TwoSum {
         int left = 0, right = nums.length - 1;
         while (left < right) {
             int sum = nums[left] + nums[right];
-            if (sum == target) {
-                return true;
+            if (sum < target) {
+                left++;
             } else if (sum > target) {
                 right--;
-            } else if (sum < target) {
-                left++;
+            } else {
+                return true;
             }
         }
         return false;
