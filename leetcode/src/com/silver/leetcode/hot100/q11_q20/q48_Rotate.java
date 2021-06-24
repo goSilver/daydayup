7c4a1cc1702003c81e1a67eaafdca88620de37ca
SONGUKU <silverchen961@foxmail.com>
2021-06-24 21:02:18 +0800

    90度反转二维数组

---
+++ leetcode/src/com/silver/leetcode/hot100/q11_q20/q48_Rotate.java
@@ -0,0 +1,30 @@
+package com.silver.leetcode.hot100.q11_q20;
+
+/**
+ * 90度反转二维数组
+ *
+ * @author csh
+ * @date 2021/6/24
+ **/
+public class q48_Rotate {
+
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        // 水平翻转
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = tmp;
+            }
+        }
+        // 对角线翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+}
