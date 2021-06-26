7641170dfbb320a2972759f8dcd5c9861be66ffe
SONGUKU <silverchen961@foxmail.com>
2021-06-26 14:09:48 +0800

    三色排序

---
+++ leetcode/src/com/silver/leetcode/hot100/q21_q30/q75_SortColors.java
@@ -0,0 +1,71 @@
+package com.silver.leetcode.hot100.q21_q30;
+
+/**
+ * 三色排序
+ * @author csh
+ * @date 2021/6/26
+ **/
+public class q75_SortColors {
+    public void sortColors1(int[] nums) {
+        int ptr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int tmp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = tmp;
+                ptr++;
+            }
+        }
+        for (int i = ptr; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                int tmp = nums[i];
+                nums[i] = nums[ptr];
+                nums[ptr] = tmp;
+                ptr++;
+            }
+        }
+    }
+
+    public void sortColors2(int[] nums) {
+        int p0 = 0, p1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                int tmp = nums[i];
+                nums[i] = nums[p1];
+                nums[p1] = tmp;
+                p1++;
+            }
+            if (nums[i] == 0) {
+                int tmp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = tmp;
+                if (p0 < p1) {
+                    tmp = nums[i];
+                    nums[i] = nums[p1];
+                    nums[p1] = tmp;
+                }
+                p0++;
+                p1++;
+            }
+        }
+    }
+
+    public void sortColors3(int[] nums) {
+        int p0 = 0, p2 = nums.length - 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            // 注意
+            if (i <= p2 && nums[i] == 2) {
+                int tmp = nums[i];
+                nums[i] = nums[p2];
+                nums[p2] = tmp;
+                p2--;
+            }
+            if (nums[i] == 0) {
+                int tmp = nums[i];
+                nums[i] = nums[p0];
+                nums[p0] = tmp;
+                p0++;
+            }
+        }
+    }
+}
