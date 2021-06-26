1d33103f1f52383d34b7ad6a1f92f79ecf44b453
SONGUKU <silverchen961@foxmail.com>
2021-06-26 17:35:52 +0800

    验证二叉搜索树

--- leetcode/src/com/silver/leetcode/hot100/q31_q40/q98_IsValidBST.java
+++ leetcode/src/com/silver/leetcode/hot100/q31_q40/q98_IsValidBST.java
@@ -19,4 +19,23 @@ public class q98_IsValidBST {
             return false;
         return helper(root.left, Integer.MIN_VALUE, root.val) && helper(root.right, root.val, Integer.MAX_VALUE);
     }
+
+    long pre = Long.MIN_VALUE;
+    public boolean isValidBST2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 访问左子树
+        if (!isValidBST2(root.left)) {
+            return false;
+        }
+        // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        // 访问右子树
+        return isValidBST2(root.right);
+    }
+
 }
