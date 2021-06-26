030d97ce76c451c22c5e288159d2319f86cf8843
SONGUKU <silverchen961@foxmail.com>
2021-06-26 22:47:48 +0800

    只出现一次的数字

--- leetcode/src/com/silver/leetcode/hot100/q31_q40/q136_SingleNumber.java
+++ leetcode/src/com/silver/leetcode/hot100/q31_q40/q136_SingleNumber.java
@@ -1,10 +1,22 @@
 package com.silver.leetcode.hot100.q31_q40;
 
 /**
+ * 只出现一次的数字
+ *
  * @author csh
  * @date 2021/6/26
  **/
 public class q136_SingleNumber {
+    /**
+     * 异或运算有以下三个性质。
+     * <p>
+     * 任何数和 00 做异或运算，结果仍然是原来的数，即 a⊕0=a。
+     * 任何数和其自身做异或运算，结果是 0，即 a⊕a=0。
+     * 异或运算满足交换律和结合律，即 a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b。
+     *
+     * @param nums
+     * @return
+     */
     public int singleNumber(int[] nums) {
         int single = 0;
         for (int num : nums) {
@@ -12,4 +24,5 @@ public class q136_SingleNumber {
         }
         return single;
     }
+
 }
