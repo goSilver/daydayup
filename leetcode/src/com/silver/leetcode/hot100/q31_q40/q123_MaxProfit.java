5a08f0ee990ca0076661f07969bc0e54e18c2266
SONGUKU <silverchen961@foxmail.com>
2021-06-28 01:05:10 +0800

    股票买卖Ⅲ

---
+++ leetcode/src/com/silver/leetcode/hot100/q31_q40/q123_MaxProfit.java
@@ -0,0 +1,20 @@
+package com.silver.leetcode.hot100.q31_q40;
+
+/**
+ * @author csh
+ * @date 2021/6/28
+ **/
+public class q123_MaxProfit {
+    public int maxProfit1(int[] prices) {
+        int n = prices.length;
+        int buy1 = -prices[0], sell1 = 0;
+        int buy2 = -prices[0], sell2 = 0;
+        for (int i = 1; i < n; i++) {
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, buy1+prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+        }
+        return sell2;
+    }
+}
