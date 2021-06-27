9199a2009a4202b388f448e586987b68fec17c6b
SONGUKU <silverchen961@foxmail.com>
2021-06-27 22:38:07 +0800

    移动零

---
+++ leetcode/src/com/silver/leetcode/hot100/q51_q60/q283_MoveZeros.java
@@ -0,0 +1,24 @@
+package com.silver.leetcode.hot100.q51_q60;
+
+/**
+ * @author csh
+ * @date 2021/6/27
+ **/
+public class q283_MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
