eff62f2765d9cdfb4d122c1026751bc5a3bf62b7
SONGUKU <silverchen961@foxmail.com>
2021-06-28 22:09:39 +0800

    最长无重复子串

--- leetcode/src/com/silver/labuladong/catalog/chapter1/doublepointer/window/LengthOfLongestSubstring.java
+++ leetcode/src/com/silver/labuladong/catalog/chapter1/doublepointer/window/LengthOfLongestSubstring.java
@@ -15,14 +15,14 @@ public class LengthOfLongestSubstring {
         int res = 0;
         while (right < s.length()) {
             // 向右扩大窗口
-            char c = s.charAt(right);
+            char c1 = s.charAt(right);
             right++;
-            window.put(c, window.getOrDefault(c, 0) + 1);
+            window.put(c1, window.getOrDefault(c1, 0) + 1);
             // 当出现重复字符，缩小窗口
-            if (window.get(c) > 1) {
-                char c1 = s.charAt(left);
+            if (window.get(c1) > 1) {
+                char c2 = s.charAt(left);
                 left++;
-                window.put(c1, window.getOrDefault(c, 0) - 1);
+                window.put(c2, window.getOrDefault(c2, 0) - 1);
             }
             // 在这里更新答案
             res = Math.max(res, right - left);
