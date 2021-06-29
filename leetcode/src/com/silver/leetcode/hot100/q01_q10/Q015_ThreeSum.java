a819a547e897a34071b67c697ba834be2a4f2a84
SONGUKU <silverchen961@foxmail.com>
2021-06-29 21:51:02 +0800

    三数之和

--- leetcode/src/com/silver/leetcode/hot100/q01_q10/Q015_ThreeSum.java
+++ leetcode/src/com/silver/leetcode/hot100/q01_q10/Q015_ThreeSum.java
@@ -24,22 +24,47 @@ public class Q015_ThreeSum {
         for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] > 0) break;
             // 注意
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1, right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     res.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
                     // 注意
-                    while(left < right && nums[left] == nums[++left]);
-                    while(left < right && nums[right] == nums[--right]);
+                    while (left < right && nums[left] == nums[++left]) ;
+                    while (left < right && nums[right] == nums[--right]) ;
 
                 } else if (sum > 0) {
                     // 注意
-                    while(left < right && nums[right] == nums[--right]);
+                    while (left < right && nums[right] == nums[--right]) ;
                 } else if (sum < 0) {
                     // 注意
-                    while(left < right && nums[left] == nums[++left]);
+                    while (left < right && nums[left] == nums[++left]) ;
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public List<List<Integer>> threeSum2(int[] arr) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 2; i++) {
+            if (arr[i] > 0) break;
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            int left = i + 1, right = arr.length - 1;
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                if (sum == 0) {
+                    res.add(new ArrayList<>(Arrays.asList(arr[i], arr[left], arr[right])));
+
+                    while (left < right && arr[left] == arr[++left]) ;
+                    while (left < right && arr[right] == arr[--right]) ;
+                } else if (sum > 0) {
+                    while (left < right && arr[right] == arr[--right]) ;
+                } else if (sum < 0) {
+                    while (left < right && arr[left] == arr[++left]) ;
                 }
             }
         }
