204fffe03ea4401ae3a5318ec8424cff969f45c1
SONGUKU <silverchen961@foxmail.com>
2021-07-03 16:22:01 +0800

    回文子串

---
+++ leetcode/src/com/silver/leetcode/hot100/q61_q100/q647_CountSubStr.java
@@ -0,0 +1,34 @@
+package com.silver.leetcode.hot100.q61_q100;
+
+/**
+ * 回文子串
+ *
+ * @author csh
+ * @date 2021/7/3
+ **/
+public class q647_CountSubStr {
+    private int res;
+
+    public int countSubstrings(String s) {
+        if (s.length() == 0) return 0;
+        for (int i = 0; i < s.length(); i++) {
+            palindrome(s, i, i);
+            palindrome(s, i, i + 1);
+        }
+        return res;
+    }
+
+    private void palindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            res++;
+            left--;
+            right++;
+        }
+    }
+
+    public static void main(String[] args) {
+        q647_CountSubStr main = new q647_CountSubStr();
+        int abc = main.countSubstrings("aaa");
+        System.out.println(abc);
+    }
+}
