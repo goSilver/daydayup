afdcc9e8dd7ebfe52f12657f27349b1710ab566e
SONGUKU <silverchen961@foxmail.com>
2021-07-03 23:12:54 +0800

    合并两个有序链表

---
+++ leetcode/src/com/silver/leetcode/hot100/review/first/q21_mergeTwoList.java
@@ -0,0 +1,26 @@
+package com.silver.leetcode.hot100.review.first;
+
+import com.silver.labuladong.temp.ListNode;
+
+/**
+ * @author csh
+ * @date 2021/7/3
+ **/
+public class q21_mergeTwoList {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
