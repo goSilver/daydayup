6aecdbdf0f72f042e4c59e934aa8944b84dc0d82
SONGUKU <silverchen961@foxmail.com>
2021-07-04 00:44:59 +0800

    合并K个有序链表

---
+++ leetcode/src/com/silver/leetcode/hot100/review/first/q23_mergeKList.java
@@ -0,0 +1,38 @@
+package com.silver.leetcode.hot100.review.first;
+
+import com.silver.labuladong.temp.ListNode;
+
+/**
+ * @author csh
+ * @date 2021/7/4
+ **/
+public class q23_mergeKList {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    public ListNode merge(ListNode[] lists, int l, int r) {
+        if (l == r) return lists[l];
+        if (l > r) return null;
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 == null ? l2 : l1;
+        return dummy.next;
+    }
+}
