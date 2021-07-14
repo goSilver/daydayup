407c885f7726f745bfec7a15980019e3c5f9fee7
SONGUKU <silverchen961@foxmail.com>
2021-07-14 23:48:54 +0800

    打家劫舍

--- leetcode/src/com/silver/leetcode/hot100/review/first/q198_rob.java
+++ leetcode/src/com/silver/leetcode/hot100/review/first/q198_rob.java
@@ -1,6 +1,8 @@
 package com.silver.leetcode.hot100.review.first;
 
 /**
+ * 打家劫舍
+ *
  * @author csh
  * @date 2021/7/5
  **/
@@ -15,5 +17,17 @@ public class q198_rob {
             second = newS;
         }
         return Math.max(first, second);
+    }
+
+    public int rob2(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[][] rob = new int[nums.length][2];
+        rob[0][0] = 0;
+        rob[0][1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            rob[i][0] = Math.max(rob[i - 1][0], rob[i - 1][1]);
+            rob[i][1] = Math.max(rob[i - 1][0] + nums[i], rob[i - 1][1]);
+        }
+        return Math.max(rob[nums.length - 1][0], rob[nums.length - 1][1]);
     }
 }
