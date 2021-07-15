ccef0c21e29ec81aa627dd58c68fe1a396b59e4d
SONGUKU <silverchen961@foxmail.com>
2021-07-16 00:12:43 +0800

    打家劫舍Ⅲ

---
+++ leetcode/src/com/silver/leetcode/hot100/review/first/q337_rob.java
@@ -0,0 +1,25 @@
+package com.silver.leetcode.hot100.review.first;
+
+import com.silver.sword4offer.TreeNode;
+
+/**
+ * 打家劫舍Ⅲ
+ *
+ * @author csh
+ * @date 2021/7/16
+ **/
+public class q337_rob {
+    public int rob(TreeNode root) {
+        int[] status = dfs(root);
+        return Math.max(status[0], status[1]);
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) return new int[]{0, 0};
+        int[] l = dfs(root.left);
+        int[] r = dfs(root.right);
+        int select = root.val + l[0] + r[0];
+        int notSel = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[]{select, notSel};
+    }
+}
