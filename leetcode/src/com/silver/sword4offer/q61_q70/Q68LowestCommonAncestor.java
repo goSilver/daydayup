9cacae5bfd6f68000e09c5eb5e346c1de3248d3b
SONGUKU <silverchen961@foxmail.com>
2021-07-18 23:44:19 +0800

    二叉树的最近公共祖先

--- leetcode/src/com/silver/sword4offer/q61_q70/Q68LowestCommonAncestor.java
+++ leetcode/src/com/silver/sword4offer/q61_q70/Q68LowestCommonAncestor.java
@@ -9,12 +9,38 @@ import com.silver.sword4offer.TreeNode;
  * @date 2021/3/21
  **/
 public class Q68LowestCommonAncestor {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if(left == null) return right;
-        if(right == null) return left;
+    /**
+     * 二叉搜索树的最近公共祖先
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor1(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor1(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor1(root.left, p, q);
+        }
+        return root;
+    }
+
+    /**
+     * 二叉树的最近公共祖先
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+        if (left == null) return right;
+        if (right == null) return left;
         return root;
     }
 
